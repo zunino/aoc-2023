5ce963e96353922c468ddd24a01852301a8b96e2
Andre Zunino <neyzunino@gmail.com>
2024-06-07 14:50:00 -0300

    Day 4 - part 2

--- d04.rs
+++ d04.rs
@@ -1,13 +1,15 @@
 use std::fs;
+use std::collections::{HashMap, VecDeque};
 
 #[derive(Debug)]
 struct Card {
-    id: u8,
     winning_numbers: Vec<u8>,
     own_numbers: Vec<u8>,
 }
 
-fn parse_card(input: &str) -> Card {
+type Cards = HashMap<u8, Card>;
+
+fn parse_card(input: &str) -> (u8, Card) {
     let (card_label, numbers) = input.split_once(":").unwrap();
     let card_id = card_label
         .split_whitespace()
@@ -23,16 +25,22 @@ fn parse_card(input: &str) -> Card {
     let mut own: Vec<u8> = own.split_whitespace().map(|n| n.parse().unwrap()).collect();
     winning.sort();
     own.sort();
-    return Card {
-        id: card_id,
+    (card_id, Card {
         winning_numbers: winning,
         own_numbers: own,
-    };
+    })
 }
 
-fn read_cards() -> Vec<Card> {
+fn read_cards() -> Cards {
     let data = fs::read_to_string("data/d04.txt").expect("Error reading data file");
-    data.lines().map(parse_card).collect()
+    let entries: Vec<(u8, Card)> = data.lines()
+        .map(parse_card)
+        .collect::<Vec<(u8, Card)>>();
+    let mut cards: Cards = HashMap::new();
+    for (id, card) in entries.into_iter() {
+        cards.insert(id, card);
+    }
+    cards
 }
 
 fn count_winning_numbers(card: &Card) -> u8 {
@@ -54,9 +62,9 @@ fn count_winning_numbers(card: &Card) -> u8 {
     count
 }
 
-fn part_1_total_points(cards: &Vec<Card>) -> u32 {
+fn part_1_total_points(cards: &Cards) -> u32 {
     let mut points = 0u32;
-    for card in cards {
+    for (_, card) in cards {
         let n_winning = count_winning_numbers(card);
         if n_winning > 0 {
             points += 2u32.pow(n_winning as u32 - 1);
@@ -65,7 +73,27 @@ fn part_1_total_points(cards: &Vec<Card>) -> u32 {
     points
 }
 
+// ==[ PART 2]=====
+
+fn part_2_total_scratch_cards(cards: &Cards) -> u32 {
+    let mut total_cards = cards.len() as u32;
+    let mut card_ids: VecDeque<u8> = cards.keys().cloned().collect();
+    while card_ids.len() > 0 {
+        let card_id = card_ids.pop_front().unwrap();
+        let card = cards.get(&card_id).unwrap();
+        let n_winning = count_winning_numbers(card);
+        if n_winning > 0 {
+            for i in 1..=n_winning {
+                card_ids.push_back(card_id + i);
+            }
+            total_cards += n_winning as u32;
+        }
+    }
+    total_cards
+}
+
 fn main() {
     let cards = read_cards();
     println!("Day 4 - part 1: {}", part_1_total_points(&cards));
+    println!("Day 4 - part 2: {}", part_2_total_scratch_cards(&cards));
 }
